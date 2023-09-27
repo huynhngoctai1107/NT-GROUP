e2596c2d6265613ad9ed1163e52563abe5833a6e
nhilhpc03821 <118969691+nhilhpc03821@users.noreply.github.com>
2023-09-27 14:45:16 +0700

    Fix Bug - Update 4

--- routes/web.php
+++ routes/web.php
@@ -32,6 +32,7 @@ use App\Http\Controllers\Client\Search\SearchController;
 use App\Http\Controllers\Client\About\AboutController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::group(['prefix' => 'admin'], function (){
     Route::get('/', [ViewDashboardController::class, 'dashboar'])->name('dashboar');
     Route::group(['prefix' => 'demand'], function (){
@@ -102,6 +103,11 @@ Route::group(['prefix' => '/'], function (){
         Route::get('/list', [ContactController::class, 'contact'])->name('contact');
     });
 });
+
+// Client Post and My account
+Route::get('/Post',[AddPostController::class,'post'])->name('post');
+
+Route::get('/Account',[AccountController::class,'account'])->name('account');
 
 
 
