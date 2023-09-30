61e88d8fc9a100583949390999b0eb4fbbe7eb5a
nhilhpc03821 <118969691+nhilhpc03821@users.noreply.github.com>
2023-09-30 22:27:39 +0700

    FEC-31 Footer - Update

--- resources/views/Client/Footer/Footer.blade.php
+++ resources/views/Client/Footer/Footer.blade.php
@@ -1,77 +1,64 @@
+<x-client.pages.subscribe></x-client.pages.subscribe>
+
 <footer class="footer_dark">
     <div class="footer_top">
         <div class="container">
-            <div class="row">
-                <div class="col-lg-3 col-md-6 col-sm-12">
-                    <div class="widget">
-                        <div class="footer_logo">
-                            <a href="#"><img src="{{asset('client/images/logo_light.png')}}" alt="logo"/></a>
+            <div class="container-fluid mt-4">
+                <div class="row mb-5 ms-md-4">
+                    <div class="col-md-4 col-sm-4 col-xs-4">
+                        <div class="footer-text">
+                            <div class="footer_logo">
+                                <a href="{{ route('index') }}"><img src="{{asset('client/images/logo.png')}}" width="40%" alt="Logo"></a>
+                                <p>Mang lại giá trị cốt lỗi trong cuộc sống</p>
+                                <ul class="widget_links">
+                                    <li class="bi bi-envelope"><a href="htai67934@mail.com "> htai67934@mail.com </a></li>
+                                    <li class="bi bi-telephone"><a href="0949 615 859 "> 0949 615 859 </a></li>
+                                </ul>
+                            </div>
                         </div>
-                        <p>If you are going to use of Lorem Ipsum need to be sure there isn't hidden of text</p>
                     </div>
-                    <div class="widget">
-                        <ul class="social_icons social_white">
-                            <li><a href="#"><i class="ion-social-facebook"></i></a></li>
-                            <li><a href="#"><i class="ion-social-twitter"></i></a></li>
-                            <li><a href="#"><i class="ion-social-googleplus"></i></a></li>
-                            <li><a href="#"><i class="ion-social-youtube-outline"></i></a></li>
-                            <li><a href="#"><i class="ion-social-instagram-outline"></i></a></li>
-                        </ul>
+                    <div class="col-md-2 col-sm-4 col-xs-4">
+                        <div class="widget">
+                            <h6 class="widget_title">Dịch vụ</h6>
+                            <ul class="widget_links">
+                                <li><a href="#">Cho thuê nhà</a></li>
+                                <li><a href="#">Cho thuê đất</a></li>
+                                <li><a href="#">Mua bán bất động sản</a></li>
+                                <li><a href="#">Tư vấn bất động sản</a></li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-                <div class="col-lg-2 col-md-3 col-sm-6">
-                    <div class="widget">
-                        <h6 class="widget_title">Useful Links</h6>
-                        <ul class="widget_links">
-                            <li><a href="#">About Us</a></li>
-                            <li><a href="#">FAQ</a></li>
-                            <li><a href="#">Location</a></li>
-                            <li><a href="#">Affiliates</a></li>
-                            <li><a href="#">Contact</a></li>
-                        </ul>
+                    <div class="col-md-2 col-sm-4 col-xs-4">
+                        <div class="widget">
+                            <h6 class="widget_title">Hỗ trợ</h6>
+                            <ul class="widget_links">
+                                <li><a href="{{ route('policy') }}">Chính sách bảo mật</a></li>
+                                <li><a href="{{ route('terms') }}">Điều khoản thoả thuận</a></li>
+                                <li><a href="#">Guide</a></li>
+                                <li><a href="#">My Account</a></li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-                <div class="col-lg-2 col-md-3 col-sm-6">
-                    <div class="widget">
-                        <h6 class="widget_title">Category</h6>
-                        <ul class="widget_links">
-                            <li><a href="#">Men</a></li>
-                            <li><a href="#">Woman</a></li>
-                            <li><a href="#">Kids</a></li>
-                            <li><a href="#">Best Saller</a></li>
-                            <li><a href="#">New Arrivals</a></li>
-                        </ul>
+                    <div class="col-md-2 col-sm-4 col-xs-4" >
+                        <div class="widget">
+                            <h6 class="widget_title">Công Ty</h6>
+                            <ul class="widget_links">
+                                <li><a href="#">Giới thiệu</a></li>
+                                <li><a href="#">Blog</a></li>
+                                <li><a href="#">Liên hệ</a></li>
+                                <li><a href="#">Partners</a></li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-                <div class="col-lg-2 col-md-6 col-sm-6">
-                    <div class="widget">
-                        <h6 class="widget_title">My Account</h6>
-                        <ul class="widget_links">
-                            <li><a href="#">My Account</a></li>
-                            <li><a href="#">Discount</a></li>
-                            <li><a href="#">Returns</a></li>
-                            <li><a href="#">Orders History</a></li>
-                            <li><a href="#">Order Tracking</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-4 col-sm-6">
-                    <div class="widget">
-                        <h6 class="widget_title">Contact Info</h6>
-                        <ul class="contact_info contact_info_light">
-                            <li>
-                                <i class="ti-location-pin"></i>
-                                <p>123 Street, Old Trafford, New South London , UK</p>
-                            </li>
-                            <li>
-                                <i class="ti-email"></i>
-                                <a href="mailto:info@sitename.com">info@sitename.com</a>
-                            </li>
-                            <li>
-                                <i class="ti-mobile"></i>
-                                <p>+ 457 789 789 65</p>
-                            </li>
-                        </ul>
+                    <div class="col-md-2 col-sm-4 col-xs-4">
+                        <div class="widget">
+                            <h6 class="widget_title">Liên kết</h6>
+                            <ul class="widget_links">
+                                <li><a href="#">Facebook</a></li>
+                                <li><a href="#">Instagram</a></li>
+                                <li><a href="#">Telegram</a></li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -81,17 +68,12 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
-                    <p class="mb-md-0 text-center text-md-start">© 2020 All Rights Reserved by Bestwebcreator</p>
+                    <p class="mb-md-0 text-center text-md-start">© Design with love © NT - GROUP 2023. All right reserved</p>
                 </div>
                 <div class="col-md-6">
                     <ul class="footer_payment text-center text-lg-end">
                         <li><a href="#"><img src="{{asset('client/images/visa.png')}}" alt="visa"></a></li>
-                        <li><a href="#"><img src="{{asset('client/images/discover.png')}}" alt="discover"></a></li>
-                        <li><a href="#"><img src="{{asset('client/images/master_card.png')}}" alt="master_card"></a>
-                        </li>
                         <li><a href="#"><img src="{{asset('client/images/paypal.png')}}" alt="paypal"></a></li>
-                        <li><a href="#"><img src="{{asset('client/images/amarican_express.png')}}"
-                                             alt="amarican_express"></a></li>
                     </ul>
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,22 @@ use App\Http\Controllers\Admin\Demand\DeleteDemandController;
 use App\Http\Controllers\Admin\Demand\EditDemandController;
 use App\Http\Controllers\Admin\Demand\ListDemandController;
 use App\Http\Controllers\Admin\Transactions\RechargeHistoryController;
-use App\Http\Controllers\Admin\Account\AddUserController;
-use App\Http\Controllers\Admin\Account\DeleteUserController;
-use App\Http\Controllers\Admin\Account\EditUserController;
-use App\Http\Controllers\Admin\Account\ListUserController;
+
+use App\Http\Controllers\Admin\User\AddUserController;
+use App\Http\Controllers\Admin\User\DeleteUserController;
+use App\Http\Controllers\Admin\User\EditUserController;
+use App\Http\Controllers\Admin\User\ListUserController;
+
 use App\Http\Controllers\Admin\Voucher\AddVoucherController;
 use App\Http\Controllers\Admin\Voucher\DeleteVoucherController;
 use App\Http\Controllers\Admin\Voucher\EditVoucherController;
 use App\Http\Controllers\Admin\Voucher\ListVoucherController;
+//admin
+use App\Http\Controllers\Admin\Posts\ListPostsController;
+use App\Http\Controllers\Admin\Posts\DeletePostsController;
+use App\Http\Controllers\Admin\Posts\EditPostsController;
+use App\Http\Controllers\Admin\Posts\AddPostsController;
+
 use App\Http\Controllers\Client\Account\AccountController;
 use App\Http\Controllers\Client\Account\FogetPasswordController;
 use App\Http\Controllers\Client\Account\LoginController;
@@ -30,6 +38,10 @@ use App\Http\Controllers\Client\Post\PostListController;
 use App\Http\Controllers\Client\Post\PostNewController;
 use App\Http\Controllers\Client\Search\SearchController;
 use App\Http\Controllers\Client\About\AboutController;
+use App\Http\Controllers\Client\Contact\ContactController;
+
+use App\Http\Controllers\Client\Docs\DocsController;
+
 use Illuminate\Support\Facades\Route;
 
 
@@ -46,10 +58,8 @@ Route::group(['prefix' => 'admin'], function (){
     });
     Route::group(['prefix' => 'category'], function (){
         Route::get('/list', [ListCategoryController::class, 'listCategory'])->name('listCategory');
-        Route::get('/delete', [DeleteCategoryController::class, 'deleteCategory'])
-             ->name('deleteCategory');
-        Route::get('/edit', [EditCategoryController::class, 'editFormCategory'])
-             ->name('editCategory');
+        Route::get('/delete', [DeleteCategoryController::class, 'deleteCategory'])->name('deleteCategory');
+        Route::get('/edit', [EditCategoryController::class, 'editFormCategory'])->name('editCategory');
         Route::post('/edit', [EditCategoryController::class, 'editCategory'])->name('editCategory');
         Route::get('/add', [AddCategoryController::class, 'addFormCategory'])->name('addCategory');
         Route::post('/add', [AddCategoryController::class, 'addCategory'])->name('addCategory');
@@ -64,8 +74,7 @@ Route::group(['prefix' => 'admin'], function (){
     });
     Route::group(['prefix' => 'voucher'], function (){
         Route::get('/list', [ListVoucherController::class, 'listVoucher'])->name('listVoucher');
-        Route::get('/delete', [DeleteVoucherController::class, 'deleteVoucher'])
-             ->name('deleteVoucher');
+        Route::get('/delete', [DeleteVoucherController::class, 'deleteVoucher'])->name('deleteVoucher');
         Route::get('/edit', [EditVoucherController::class, 'editFormVoucher'])->name('editVoucher');
         Route::post('/edit', [EditVoucherController::class, 'editVoucher'])->name('editVoucher');
         Route::get('/add', [AddVoucherController::class, 'addFormVoucher'])->name('addVoucher');
@@ -74,6 +83,14 @@ Route::group(['prefix' => 'admin'], function (){
     Route::group(['prefix' => 'transactions'], function (){
         Route::get('/list', [RechargeHistoryController::class, 'listRechargeHistory'])
              ->name('listRechargeHistory');
+    });
+    Route::group(['prefix' => 'posts'], function (){
+        Route::get('/list', [ListPostsController::class, 'listPosts'])->name('listPosts');
+        Route::get('/delete', [DeletePostsController::class, 'deletePosts'])->name('deletePosts');
+        Route::get('/edit', [EditPostsController::class, 'editFormPosts'])->name('editPosts');
+        Route::post('/edit', [EditPostsController::class, 'editPosts'])->name('editPosts');
+        Route::get('/add', [AddPostsController::class, 'addFormPosts'])->name('addPosts');
+        Route::post('/add', [AddPostsController::class, 'addPosts'])->name('addPosts');
     });
 });
 
@@ -93,7 +110,7 @@ Route::group(['prefix' => '/'], function (){
         Route::get('/add', [AddPostController::class, 'post'])->name('postAdd');
         Route::get('/new', [PostNewController::class, 'postNew'])->name('postNew');
         Route::get('/vip', [PostNewController::class, 'postList'])->name('postList');
-        Route::get('/delete', [PostNewController::class, 'postDelete'])->name('postDelete');
+        Route::get('/delete',[PostNewController::class, 'postDelete'])->name('postDelete');
         Route::get('/list', [PostListController::class, 'listPost'])->name('listPost');
     });
     Route::group(['prefix' => 'search'], function (){
@@ -102,6 +119,11 @@ Route::group(['prefix' => '/'], function (){
     Route::group(['prefix' => 'contact'], function (){
         Route::get('/list', [ContactController::class, 'contact'])->name('contact');
     });
+    Route::group(['prefix' => 'docs'], function (){
+        Route::get('/terms',  [DocsController::class, 'docsterms'])->name('terms');
+        Route::get('/policy', [DocsController::class, 'docspolicy'])->name('policy');
+    });
+
 });
 
 
