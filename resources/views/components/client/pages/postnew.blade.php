1fac9c11316b5f91b87fce9c1f786a7abbf6c028
htai67934@gmail.com <htai67923@gmail.com>
2023-10-26 13:47:45 +0700

    BE-37 Sửa nội dung đăng tin

---
+++ resources/views/components/client/pages/postnew.blade.php
@@ -0,0 +1,17 @@
+<tr class="text-center" style="font-size:16px !important ">
+    <th colspan="1" >&nbsp;Ảnh</th>
+    <th colspan="2" rowspan="2" >Tiêu đề</th>
+    <th colspan="1" >Giá</th>
+ 
+    <th colspan="2">Địa chỉ</th>
+     <th colspan="2">Trạng thái</th>
+    <th colspan="2">Xem</th>
+    
+    <th colspan="2">Nghiệp vụ</th>
+</tr>
+<style>
+
+tr th{
+        font-size: 15px  !important; 
+    }
+</style>
